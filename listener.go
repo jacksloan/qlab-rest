0cbdc65d02fc6ffef045a08f7f13c289a5aefc61
Jack Sloan <jbsloan10@gmail.com>
2022-03-16 21:17:35 -0500

    (bad) allocate the maximum datagram size for every read from UDP... figure out a better way later maybe

--- listener.go
+++ listener.go
@@ -13,22 +13,21 @@ func ReceiveFromQLab() {
 
 	defer conn.Close()
 
-	conn.SetReadBuffer(1048576)
+	conn.SetReadBuffer(65_535)
 
 	if err != nil {
 		panic(err)
 	}
 
-	i := 0
 	for {
-		i++
-		buf := make([]byte, 1024)
-		rlen, _, _ := conn.ReadFromUDP(buf[:])
-		go handlePacket(buf, rlen, i)
+		// read the maximum datagram size
+		buf := make([]byte, 65_535)
+		rlen, address, _ := conn.ReadFromUDP(buf[:])
+		fmt.Println(address)
+		go handlePacket(buf, rlen)
 	}
 }
 
-func handlePacket(buf []byte, rlen int, count int) {
+func handlePacket(buf []byte, rlen int) {
 	fmt.Println(string(buf[0:rlen]))
-	// fmt.Println(count)
 }
