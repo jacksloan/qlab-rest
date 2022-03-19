ddcadae0a17f6dec08248061053de4825540d2ee
jacksloan <jbsloan10@gmail.com>
2022-03-18 21:42:08 -0500

    add swagger-ui for easy osc message testing

--- server/main.ts
+++ server/main.ts
@@ -1,6 +1,8 @@
 //I solve the problem at this place
 import express from "express";
 import * as path from "path";
+import swaggerUi from "swagger-ui-express";
+import fs from "fs";
 import { handleOscCommand } from "./handlers/osc-handler";
 import { handleServerSpa as handleServeSpa } from "./handlers/spa-handler";
 import { Osc } from "./services/osc";
@@ -13,8 +15,16 @@ const osc = new Osc({
     console.log(`app listening on ${port}`);
   },
 });
+
 app.use(express.json({ strict: false }));
 app.use("/api(/*)?", handleOscCommand(osc));
+
+const swaggerPath = path.join(__dirname, "..", "spec.json");
+const swaggerDocument = fs.readFileSync(swaggerPath);
+app.use("/api-docs", express.static(path.join(__dirname, "..", "api-docs")));
+app.use("/api-docs", swaggerUi.serve);
+app.get("/api-docs", swaggerUi.setup(JSON.parse(swaggerDocument.toString())));
+
 app.use(express.static(path.join(__dirname, "..", "client")));
 app.use("/", handleServeSpa);
 
