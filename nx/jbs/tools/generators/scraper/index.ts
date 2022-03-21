800807e1dad97ee9c0584219ee7af58901a75ba9
jacksloan <jbsloan10@gmail.com>
2022-03-21 00:25:37 -0500

    add initial codegen

--- nx/jbs/tools/generators/scraper/index.ts
+++ nx/jbs/tools/generators/scraper/index.ts
@@ -1,13 +1,14 @@
 import {
-  logger,
   formatFiles,
   installPackagesTask,
+  joinPathFragments,
+  logger,
   readProjectConfiguration,
-  Tree,
+  Tree
 } from '@nrwl/devkit';
 import { libraryGenerator } from '@nrwl/workspace/generators';
 import { OpenAPI } from 'openapi-types';
-import * as path from 'path';
+import { codegen } from './src/codegen';
 import { convert } from './src/convert';
 import { OscCommand } from './src/model';
 import { scrapeCommands } from './src/scrape';
@@ -23,8 +24,8 @@ export default async function (
   const doc: OpenAPI.Document = convert(commands);
   const fileName = schema.filename || 'openapi.json';
   const sourceRoot = readProjectConfiguration(tree, schema.name).root;
-  tree.write(path.join(sourceRoot, fileName), JSON.stringify(doc));
-
+  tree.write(joinPathFragments(sourceRoot, fileName), JSON.stringify(doc));
+  codegen(tree, schema, doc);
   await formatFiles(tree);
   return () => installPackagesTask(tree);
 }
