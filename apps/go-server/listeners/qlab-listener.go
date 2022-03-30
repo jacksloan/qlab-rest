4ea1951e2309ce4477548a9bd550d5a43472ca3c
jacksloan <jbsloan10@gmail.com>
2022-03-30 15:36:31 -0500

    qlab listener send replies on channel

--- apps/go-server/listeners/qlab-listener.go
+++ apps/go-server/listeners/qlab-listener.go
@@ -4,9 +4,10 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"strings"
 )
 
-func listenForQlab(observers map[string][]chan string) {
+func ListenForQlab(observers map[string]map[string]chan string) {
 	conn, err := net.ListenUDP("udp", &net.UDPAddr{
 		Port: 53001,
 		IP:   net.ParseIP("127.0.0.1"),
@@ -23,13 +24,27 @@ func listenForQlab(observers map[string][]chan string) {
 	for {
 		// TODO: review if necessary to read the maximum datagram size
 		buf := make([]byte, 65_535)
-		rlen, address, _ := conn.ReadFromUDP(buf[:])
-		fmt.Println(address)
+		rlen, _, _ := conn.ReadFromUDP(buf[:])
 		go handleUDPPacket(buf, rlen, observers)
 	}
 }
 
-func handleUDPPacket(buf []byte, rlen int, observers map[string][]chan string) {
+func handleUDPPacket(buf []byte, rlen int, observers map[string]map[string]chan string) {
 	packet := string(buf[0:rlen])
-	fmt.Println("packet =", packet)
+	res := strings.Split(packet, ",s")
+	address := strings.Trim((res[0]), "\x00") // remove null characters
+	body := strings.Trim((res[1]), "\x00")    // remove null characters
+	obs := observers[address]
+
+	if obs == nil {
+		fmt.Println("no listeners for address", address)
+		return
+	}
+	for key, ch := range obs {
+		// send reply to the channel and remove it from the map
+		go func(key string, ch chan string) {
+			ch <- body
+			delete(obs, key)
+		}(key, ch)
+	}
 }
