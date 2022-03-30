deb67b8cc9d75598e1494ccbe063fee94b4ad83b
jacksloan <jbsloan10@gmail.com>
2022-03-30 15:40:53 -0500

    add test qlab request/response

--- apps/go-server/main.go
+++ apps/go-server/main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"embed"
+	"fmt"
 	"goodwin/apps/go-server/handlers"
+	"goodwin/apps/go-server/listeners"
 	"log"
 	"net/http"
 	"time"
 
+	"github.com/google/uuid"
 	"github.com/gorilla/mux"
 	"github.com/hypebeast/go-osc/osc"
 )
@@ -36,6 +39,35 @@ func main() {
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
 	}
+	// test qlab command/reply
+	go func() {
+		observers := make(map[string]map[string]chan string)
+		go listeners.ListenForQlab(observers)
+		reply := sendCommand("/workspaces", observers)
+		fmt.Println("response =", reply)
+	}()
+
 	log.Println("listing on port", port)
 	log.Fatal(srv.ListenAndServe())
+}
+
+func sendCommand(command string, observers map[string]map[string]chan string) string {
+	oscClient.Send(osc.NewMessage(command))
+	observer := make(chan string)
+	replyFormat := "/reply" + command
+
+	if observers[replyFormat] == nil {
+		observers[replyFormat] = make(map[string]chan string)
+	}
+	replyKey := uuid.NewString()
+	observers[replyFormat][replyKey] = observer
+
+	select {
+	case res := <-observer:
+		return res
+	case <-time.After(3 * time.Second):
+		// cleanup observers on timeout
+		delete(observers[replyFormat], replyKey)
+		return "timed out listening for " + command
+	}
 }
