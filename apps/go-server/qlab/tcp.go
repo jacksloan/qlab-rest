18fce9d1e0826d9804c7873e7411e0df7c67dcf1
jacksloan <jbsloan10@gmail.com>
2022-03-31 23:37:27 -0500

    create QlabTcpClient type

--- apps/go-server/qlab/tcp.go
+++ apps/go-server/qlab/tcp.go
@@ -1,40 +1,57 @@
 package qlab
 
 import (
+	"fmt"
+	"goodwin/apps/go-server/db"
+	"log"
 	"net"
-	"os"
+	"time"
 
 	"github.com/Lobaro/slip"
 	"github.com/hypebeast/go-osc/osc"
 )
 
-func tcp() {
-	servAddr := "127.0.0.1:53000"
-	tcpAddr, err := net.ResolveTCPAddr("tcp", servAddr)
-	handleError("ResolveTCPAddr failed:", err)
-
-	conn, err := net.DialTCP("tcp", nil, tcpAddr)
-	handleError("Dial failed:", err)
-
-	w := slip.NewWriter(conn)
-	p := osc.NewMessage("/workspaces")
-	b, err := p.MarshalBinary()
-	handleError("Failed to marshal binary:", err)
-	error := w.WritePacket(b)
-	handleError("Failed to write packet:", error)
-
-	r := slip.NewReader(conn)
-	reply, _, err := r.ReadPacket()
-	handleError("Failed to read packet", err)
-
-	println("reply from server=", string(reply))
-
-	conn.Close()
+type QlabTcpClient struct {
+	w *slip.Writer
+	c *db.Channels
 }
 
-func handleError(prefix string, err error) {
-	if err != nil {
-		println(prefix, err.Error())
-		os.Exit(1)
+func NewTcpClient() *QlabTcpClient {
+	return &QlabTcpClient{
+		c: db.New(),
 	}
+}
+
+func (q *QlabTcpClient) Listen(isReady chan bool) {
+	conn, err := net.Dial("tcp", "127.0.0.1:53000")
+	if err != nil {
+		log.Fatal("Failed to dial tcp network 127.0.0.1:53000", err)
+	}
+	defer conn.Close()
+
+	q.w = slip.NewWriter(conn)
+	isReady <- true
+
+	for {
+		r := slip.NewReader(conn)
+		reply, _, err := r.ReadPacket()
+		if err != nil {
+			fmt.Println("error reading packet, sleeping for 200ms")
+			time.Sleep(200 * time.Millisecond)
+		}
+		fmt.Println(string(reply))
+	}
+}
+
+func (q *QlabTcpClient) Send(command string) error {
+	p := osc.NewMessage(command)
+	// TODO allow args
+	b, err := p.MarshalBinary()
+	if err != nil {
+		return err
+	}
+	if err := q.w.WritePacket(b); err != nil {
+		return err
+	}
+	return nil
 }
