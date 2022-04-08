aeaff18aa0faa30ad79e479856bb7d423663bd3b
jacksloan <jbsloan10@gmail.com>
2022-04-07 19:28:00 -0500

    make swagger-proxy the only server app

--- apps/qlab-proxy/public/README.md
+++
@@ -1,7 +0,0 @@
-### Just a placeholder
-
-This directory needs to exist in order for `go:embed` to work during development
-
-In production, the web app will be served at http://localhost:5000
-
-For development, run `npm start` and navigate to http://localhost:3000
--- apps/swagger-proxy/main.go
+++ apps/swagger-proxy/main.go
@@ -6,17 +6,23 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/jacksloan/qlab-rest/libs/proxy"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
-//go:embed all:swagger/*
-var files embed.FS
+var (
+	//go:embed all:public/*
+	files embed.FS
+
+	host = flag.String("host", "127.0.0.1", "host name")
+	port = flag.Int("port", 5000, "port number")
+)
 
 func main() {
-	port := flag.Int("port", 5001, "port number")
 	flag.Parse()
 
 	log.Println("searching QLab instances")
@@ -29,8 +35,20 @@ func main() {
 	router := mux.NewRouter()
 	path := "/api"
 	router.PathPrefix(path).Handler(proxy.HandleOsc(tcpClient, path))
-	router.PathPrefix("/").HandlerFunc(proxy.HandleStatic(files, "swagger"))
+	router.PathPrefix("/").HandlerFunc(proxy.HandleStatic(files, "public"))
 
-	log.Printf("listing at http://localhost:%d", *port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf("127.0.0.1:%d", *port), router))
+	headersOk := handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"})
+	originsOk := handlers.AllowedOrigins([]string{"*"})
+	methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"})
+	cors := handlers.CORS(headersOk, originsOk, methodsOk)
+
+	srv := &http.Server{
+		Handler:      cors(router),
+		Addr:         fmt.Sprintf("%s:%d", *host, *port),
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Printf("listening at http://%s:%d", proxy.GetOutboundIP(), *port)
+	log.Fatal(srv.ListenAndServe())
 }
